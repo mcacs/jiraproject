24aec58c1dfbb08e1799845242f53f6ababd095b
mcacs <marilia.silva@ee.ufcg.edu.br>
2023-08-27 17:56:02 -0300

    Função para a operação de Divisão

--- calculadora.c
+++ calculadora.c
@@ -14,3 +14,13 @@ float subtracao(float a, float b) {
 float multiplicacao(float a, float b) {
     return a * b;
 }
+
+// Função para realizar a divisão
+float divisao(float a, float b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        printf("Erro: Divisão por zero!\n");
+        return 0.0;
+    }
+}
